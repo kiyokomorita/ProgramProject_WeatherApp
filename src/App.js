70d291f2df0f40fb3343b49c7500b641973838b5
Kiyoko <kiyoko113@yahoo.co.jp>
2022-03-25 13:51:05 -0700

    program project

--- src/App.js
+++ src/App.js
@@ -13,27 +13,30 @@ class App extends Component{
     high: "",
     low:"",
     icon:"",
-    isRaining:""
+    isRaining:"",
+    searchCityInput:"",
   }
 
   // this method will be called after a change in state or change in props
-  componentDidUpdate(prevProps, prevState){
-    console.log("The component was updated");
-    if(this.state.weather !== prevState.weather){
-      const isRaining = this.state.weather.includes("rain");
-      if(isRaining){
-        this.setState({
-          isRaining:"Rain rain go away!"
-        })
-      }
-    }
-  }
+  // componentDidUpdate(prevProps, prevState){
+  //   console.log("The component was updated");
+  //   if(this.state.weather !== prevState.weather){
+  //     const isRaining = this.state.weather.includes("rain");
+  //     if(isRaining){
+  //       this.setState({
+  //         isRaining:"Rain rain go away!"
+  //       })
+  //     }
+  //   }
+  // }
 
   searchCity=(event)=>{
     event.preventDefault();
-    const city=document.querySelector('#city').value
+    // const city=document.querySelector('#city').value
+    const city = this.state.searchCityInput
     this.getCityWeather(city)
     this.getCityImage(city)
+    this.setState({ searchCityInput: '' })
   }
 
   getCityWeather = (city)=>{
@@ -41,12 +44,13 @@ class App extends Component{
     // native fetch
     // fetch(url).then(res =>res.json()).then(result=>console.log(result)).catch()
     axios(url).then((response) =>{
+      document.body.style.backgroundImage="url(image:response.data[0].image.url)"
       console.log(response.data)
       this.setState({
-        temp:response.data.main.temp,
+        temp:response.data.main.temp.toFixed(1),
         weather:response.data.weather[0].description,
-        high:response.data.main.temp_max,
-        low: response.data.main.temp_min,
+        high:response.data.main.temp_max.toFixed(1),
+        low: response.data.main.temp_min.toFixed(1),
         cityName: response.data.name, 
         icon: response.data.weather[0].icon
       })
@@ -59,16 +63,24 @@ class App extends Component{
     const options = {
       method: 'GET',
       url: 'https://google-image-search1.p.rapidapi.com/',
-      params: {keyword: city, max: '10'},
+      params: {keyword: city, max: '5'},
       headers: {
         'x-rapidapi-host': 'google-image-search1.p.rapidapi.com',
         'x-rapidapi-key': '2813eb5737msh7eb31687acb9d2cp1a8ea2jsn1e273e3d2fe2'
       }
     };
     
+// for(let i=0; i<10; i++){
+//   const random10= Math.floor(Math.random()*10);
+
+// }
+
+   
+   const random5 = Math.floor(Math.random()*5)
+
     axios.request(options).then((response) =>{
       this.setState({
-        image:response.data[0].image.url
+        image:response.data[random5].image.url
       })
       console.log(response.data)
       
@@ -78,19 +90,30 @@ class App extends Component{
     });
 
   }
+
   
+
 
   render(){
     // const imgUrl='https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg/2560px-La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg'
     return (
       <div className="App">
+        <div className='search-area'>
         <h1>Weather App</h1>
         <div>
+          
           <form onSubmit={this.searchCity}>
-            <input type="text" name="city" id="city" placeholder="Enter a City Name"/>
+            <input 
+              type="text" 
+              name="city" 
+              id="city" 
+              placeholder="Enter a City Name"
+              value={this.state.searchCityInput}
+              onChange={(e) => this.setState({ searchCityInput: e.target.value })}
+            />
           </form>
         </div>
-        <h4>{this.state.isRaining}</h4>
+      
         <hr/>
         {this.state.cityName && (<WeatherDetails 
           cityName={this.state.cityName}
@@ -99,11 +122,12 @@ class App extends Component{
           low={this.state.low}
           weather={this.state.weather}
           icon={this.state.icon}
-          image={this.state.image}
+          // image={this.state.image}
 
         />)}
         {/* {this.state.image} */}
-        {/* <img src={this.state.image}/> */}
+        </div>
+        <img className="city-background" src={this.state.image} alt=""/>
       </div>
     );
 
--- src/WeatherDetails.jsx
+++ src/WeatherDetails.jsx
@@ -7,10 +7,14 @@ class WeatherDetails extends Component{
     const iconUrl = `http://openweathermap.org/img/w/${this.props.icon}.png`
     return(
       <div>
-        <h4>{this.props.cityName} : {this.props.temp}</h4>
+        
+        <h4>{this.props.cityName} : {this.props.temp}°C</h4>
         <p>High:{this.props.high} -Low: {this.props.low}</p>
         <p>{this.props.weather} <img src={iconUrl} alt="icon" /></p>
-        <img src={this.props.image} alt="" />
+        
+        {/* <img src={this.props.image} alt="" /> */}
+       
+        
        
       </div>
     )
